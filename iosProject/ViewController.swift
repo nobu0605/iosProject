e1a4b02f54355e3ffb530635806dea2b56cb6bcb
Suzuki Takanobu <nobublack555@gmail.com>
2018-12-27 23:49:02 +0900

    Initial Commit

---
+++ iosProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosProject
+//
+//  Created by 鈴木孝伸 on 2018/12/27.
+//  Copyright © 2018年 鈴木孝伸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
